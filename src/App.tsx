3acdfd9df466fe29d333a0bdc09b0c6d56d497e8
Vitor Sousa <vitor.silva@vetta.digital>
2023-09-02 12:33:17 -0300

    feat: Resolve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from './components/ThemeProvider';
 import { queryClient, QueryClientProvider } from './libs/query';
 import { GlobalStyle } from './styles/global';
-import theme from './styles/theme';
-
 import { PrivateRoutes, PublicRoutes } from './routes';
 
 function App() {
