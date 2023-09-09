d60af1ee833c90152fcd7a1014970318a4f3245a
Cleidison Silva <cleidison.dev@gmail.com>
2023-09-09 19:38:49 -0300

    feat: Define style guide tokens type

--- src/styles/theme/styled.d.ts
+++ src/styles/theme/styled.d.ts
@@ -3,6 +3,15 @@ import 'styled-components';
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
-    'gray-800': string;
+    colors: {
+      primary: string;
+      white: string;
+      blackFont: string;
+    },
+    fonts: {
+      body: string;
+      brand: string;
+    }
   }
 }
+
