a20e8112fb8357415eed48773ff63e29e2038639
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2023-09-08 21:25:07 -0300

    color amended + about us description added

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,20 +1,57 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
-	height: 288px;
 	flex-shrink: 0;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	row-gap: 26px;
+	background-color: #FAA613;
+	padding: 25px 0px 45.41px 0px;
 `
+export const AboutUs = styled.article`
+	font-family: Inter;
+	color: #000;
+	font-family: Inter;
+	font-size: 30px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: normal;
+	align-items: center;
+	justify-content: center;
+	margin-bottom: 61px;
+	display: flex;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
+	padding: 0px 140px 0px 140px;
+`
+
+export const AboutUsTitle = styled.h1`
+	color: #1E1E1E
+	text-align: center;
+	font-family: Inter;
+	font-size: 35px;
+	font-style: normal;
+	font-weight: 700;
+	line-height: normal;
+`
+
+export const Line = styled.div`
+	stroke-width: 2px;
+	stroke: #000;
+	width: 770px;
+	height: 0px;
+	flex-shrink: 0;
+`
+
 export const BrandName = styled.h1`
 	color: #1E1E1E;
 	font-family: Staatliches;
 	font-size: 142.273px;
 	font-style: normal;
 	line-height: normal;
+	margin-top: 24px;
 `
 
 export const LinksFooterContainer = styled.div`
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,12 +3,22 @@ import * as styled from "./Footer.styles"
 export function Footer() {
 	return (
 		<styled.Container>
+			<styled.AboutUsTitle>
+				Sobre nós
+			</styled.AboutUsTitle>
+			<styled.AboutUs>
+				<br/>
+				Na Hamber, nossa missão é simples, mas poderosa: conectar os amantes de comida a uma variedade diversificada de restaurantes incríveis e suas deliciosas iguarias, tudo através de uma plataforma fácil de usar. Trabalhamos arduamente para facilitar o acesso a uma ampla seleção de restaurantes e oferecemos uma experiência de pedido online sem complicações.
+			</styled.AboutUs>
+			<svg xmlns="http://www.w3.org/2000/svg" width="770" height="2" viewBox="0 0 770 2" fill="none">
+				<path d="M0 1H770" stroke="black" width="770" stroke-width="2"/>
+			</svg>
 			<styled.BrandName>
 				HAMBER
 			</styled.BrandName>
 			<styled.LinksFooterContainer>
 				<span>
-				CNPJ: XXXXXXXXXXXXX
+					CNPJ: XXXXXXXXXXXXX
 				</span>
 				<a href="#">Termos e condições / Política de Privacidade</a>
 				<a href="#">FAQ</a>
