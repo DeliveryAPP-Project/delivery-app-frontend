5f403fb22748d91a254cee80c5419c720f8b8ab4
Cleidison Silva <cleidison.dev@gmail.com>
2023-09-09 21:06:31 -0300

    feat: Use Banner component on Home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,11 @@
-import { useHomeHook } from './Home.hook';
+import Banner from '../Banner';
+
 import * as styled from './Home.styles';
 
 export function Home() {
-  const { count, handleDecrement, handleIncrement } = useHomeHook();
-
   return (
     <styled.Container>
-      <strong>Delivery app</strong>
-      <styled.Content>
-        <button onClick={handleIncrement}>increment</button>
-        <button onClick={handleDecrement}>decrement</button>
-        <strong>count: {count}</strong>
-      </styled.Content>
+      <Banner />
     </styled.Container>
   );
 }
