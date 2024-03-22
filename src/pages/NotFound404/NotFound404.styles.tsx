3af0f8acf401c9dd4e0311db49fcc321958e4663
leandrofn3 <lf1938833@gmail.com>
2024-03-22 19:39:49 -0300

    criando a pagina de notfound404, e fazendo a funcionalidade dela para todas as rotas não existente do sistema.

---
+++ src/pages/NotFound404/NotFound404.styles.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const NotFoundContainer = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    flex-direction: column;
+`;
+
+export const NotfoundH1 = styled.h1`
+    font-size: 150px;
+    font-family: "Staatliches", sans-serif;
+    color: #1E1E1E;
+`;
+
+export const NotfoundP = styled.p`
+    font-size: 120px;
+    font-family: "Staatliches", sans-serif;
+    color: #1E1E1E;
+`;
+
+
--- src/routes/public.routes.tsx
+++ src/routes/public.routes.tsx
@@ -5,11 +5,13 @@ import Faq from '../pages/Faq';
 import PurchaseConfirmation from '../pages/PurchaseConfirmation';
 import Layout from '../components/Layout';
 import Restaurants from '../pages/Restaurants';
+import NotFound404 from '../pages/NotFound404';
 
 export const publicRoutes = createBrowserRouter([
 	{
 		path: '/',
 		element: <Layout />,
+		errorElement: <NotFound404 />,
 		children: [
 			{ path: '/', element: <Home /> },
 			{ path: '/restaurantes', element: <Restaurants /> },
