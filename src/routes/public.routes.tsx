f7faa00adb0c611f231ba09523b175db40a97efe
Cleidison Silva <cleidison.dev@gmail.com>
2024-03-26 08:37:42 -0300

    refactor: mudança na forma de exportar as rotas públicas. Dessa forma evitar erro no redirecionamento pelo modal

--- src/routes/public.routes.tsx
+++ src/routes/public.routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Faq from '../pages/Faq';
@@ -7,16 +7,30 @@ import Layout from '../components/Layout';
 import Restaurants from '../pages/Restaurants';
 import Restaurant from '../pages/Restaurant';
 
-export const publicRoutes = createBrowserRouter([
-	{
-		path: '/',
-		element: <Layout />,
-		children: [
-			{ path: '/', element: <Home /> },
-			{ path: '/restaurantes', element: <Restaurants /> },
-			{ path: '/restaurantes/:id', element: <Restaurant /> },
-			{ path: '/faq', element: <Faq /> },
-			{ path: '/confirme-sua-compra', element: <PurchaseConfirmation /> },
-		],
-	},
-]);
+// export const publicRoutes = createBrowserRouter([
+// 	{
+// 		path: '/',
+// 		element: <Layout />,
+// 		children: [
+// 			{ path: '/', element: <Home /> },
+// 			{ path: '/restaurantes', element: <Restaurants /> },
+// 			{ path: '/restaurantes/:id', element: <Restaurant /> },
+// 			{ path: '/faq', element: <Faq /> },
+// 			{ path: '/confirme-sua-compra', element: <PurchaseConfirmation /> },
+// 		],
+// 	},
+// ]);
+
+export function PublicRoutes() {
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path='restaurantes' element={<Restaurants />}/>
+				<Route path='restaurantes/:id' element={<Restaurant />}/>
+				<Route path='faq' element={<Faq />}/>
+				<Route path='conclua-sua-compra' element={<PurchaseConfirmation />}/>
+			</Route>
+		</Routes>
+	);
+}
