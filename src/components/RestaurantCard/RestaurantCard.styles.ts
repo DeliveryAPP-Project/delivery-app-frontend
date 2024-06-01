d1186725864579c162783c7b1c11a65b299c0c07
leandrofn3 <lf1938833@gmail.com>
2024-05-31 23:46:10 -0300

    ajustando o card para as estrelas ficassem no mesmo padrão

--- src/components/RestaurantCard/RestaurantCard.styles.ts
+++ src/components/RestaurantCard/RestaurantCard.styles.ts
@@ -4,13 +4,10 @@ export const Container = styled.div`
 	max-width: 61.8rem;
 	width: 100%;
 	height: 22.7rem;
-
 	display: flex;
 	align-items: center;
-	gap: 3rem;
-
+	gap: 1rem;
 	padding: 2rem;
-
 	border-radius: 2rem;
 	box-shadow: 0 0 1rem rgba(0, 0, 0, 0.25);
 `;
@@ -26,31 +23,33 @@ export const Image = styled.img`
 export const InfoContainer = styled.div`
 	display: flex;
   flex-direction: column;
-  justify-content: space-around;
-
-	height: 100%;
+  justify-content: center;
+  align-items: center;
+	height: 200px;
 `;
 
 export const InfoContent = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 1rem;
-  
+  flex-direction:column;
+  justify-content: center;
+  flex-wrap: wrap;
   font-size: 2rem;
+  width: 190px;
 
   h3 {
     font-weight: 700;
-    width: 50%;
+    width: 100%;
+    margin-bottom: 80px;
   }
+
+
 
   div {
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: .4rem;
+    gap: 3px;
     width: 50%;
-
     span {
       font-weight: 700;
     }
@@ -69,9 +68,16 @@ export const InfoContent = styled.div`
     height: 5.3rem;
     border-radius: 5rem;
     box-shadow: 0 .4rem .4rem rgba(0, 0, 0, 0.25);
-
+    
     display: flex;
     align-items: center;
     justify-content: center;
   }
 `;
+
+export const InfoContent2 = styled(InfoContent)`
+  align-items: center;
+  div {
+    margin-bottom: 50px;
+  }
+`
