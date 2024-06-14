3663d301d07fd2cfcc2a43e3aec87e786bc9ad89
Cleidison Silva <cleidison.dev@gmail.com>
2024-06-14 09:21:08 -0300

    feat: add new order form send

--- src/pages/PurchaseConfirmation/index.tsx
+++ src/pages/PurchaseConfirmation/index.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FormProvider, useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
 import { z } from 'zod';
 
 import bannerImg from '../../assets/banner-image-purchase-confirmation-page.png';
@@ -8,9 +9,11 @@ import brandImg from '../../assets/logo.svg';
 import Form from '../../components/Form';
 
 import * as styled from './PurchaseConfirmation.styles';
+import { useCart } from '../../context/cartContext';
+import { api } from '../../service/api';
 
 const purchaseConfirmationSchema = z.object({
-	fullName: z
+	client_name: z
 		.string()
 		.trim()
 		.nonempty('Este campo é obrigatório')
@@ -20,28 +23,55 @@ const purchaseConfirmationSchema = z.object({
 				.map((word) => word)
 				.join(' ');
 		}),
-	phone: z.string().trim().nonempty('Este campo é obrigatório'),
-	address: z.string().trim().nonempty('Este campo é obrigatório'),
-	number: z.string().trim().nonempty('Este campo é obrigatório'),
-	complement: z.string().trim().optional(),
-	neighborhood: z.string().trim().nonempty('Este campo é obrigatório'),
-	cep: z.string().trim().nonempty('Este campo é obrigatório'),
+	client_cellphone: z.string().trim().nonempty('Este campo é obrigatório'),
+	client_address: z.string().trim().nonempty('Este campo é obrigatório'),
+	client_address_number: z.string().trim().nonempty('Este campo é obrigatório'),
+	client_address_complement: z.string().trim().optional(),
+	client_address_neighborhood: z.string().trim().nonempty('Este campo é obrigatório'),
+	client_zip_code: z.string().trim().nonempty('Este campo é obrigatório'),
 });
 
 type IPurchaseConfirmation = z.infer<typeof purchaseConfirmationSchema>;
 
 export default function PurchaseConfirmation() {
+	const { cart } = useCart();
+
 	const purchaseForm = useForm<IPurchaseConfirmation>({
 		resolver: zodResolver(purchaseConfirmationSchema),
 	});
 
 	const {
 		handleSubmit,
-		formState: { errors },
+		formState: { errors }
 	} = purchaseForm;
 
-	function handlePurchaseConfirmation(data: IPurchaseConfirmation) {
-		console.log(data);
+	const handleNewOrderMutation = useMutation({
+		mutationFn: async (order: IOrder) => {
+			const response = await api.post('/orders/', order)
+			return response.data;
+		}
+	})
+
+	async function handlePurchaseConfirmation(data: IPurchaseConfirmation) {
+		const products: { product_id: number; quantity: number }[] = []
+
+		cart.forEach(item => {
+			products.push({ product_id: item.product.id, quantity: item.quantity })
+		})
+
+		const order: IOrder = {
+			client_name: data.client_name,
+    	client_cellphone: data.client_cellphone,
+	    client_address: data.client_address,
+  	  client_address_number: data.client_address_number,
+    	client_address_complement: data.client_address_complement,
+    	client_address_neighborhood: data.client_address_neighborhood,
+    	client_zip_code: data.client_zip_code,
+    	restaurant_id: cart[0].product.restaurant_id,
+    	products: products
+		}
+		
+		await handleNewOrderMutation.mutateAsync(order)
 	}
 
 	return (
@@ -62,64 +92,64 @@ export default function PurchaseConfirmation() {
 						<styled.FormLine>
 							<div>
 								<Form.Field>
-									<Form.Label htmlFor="fullName">Nome completo:</Form.Label>
-									<Form.Input name="fullName" inputSize="600px" />
+									<Form.Label htmlFor="client_name">Nome completo:</Form.Label>
+									<Form.Input name="client_name" inputSize="600px" />
 								</Form.Field>
-								{errors.fullName && <span>{errors.fullName.message}</span>}
+								{errors.client_name && <span>{errors.client_name.message}</span>}
 							</div>
 
 							<div>
 								<Form.Field>
-									<Form.Label htmlFor="phone">Telefone:</Form.Label>
-									<Form.Input inputSize="300px" name="phone" />
+									<Form.Label htmlFor="client_cellphone">Telefone:</Form.Label>
+									<Form.Input inputSize="300px" name="client_cellphone" />
 								</Form.Field>
-								{errors.phone && <span>{errors.phone.message}</span>}
-							</div>
-						</styled.FormLine>
-
-						<styled.FormLine>
-							<div>
-								<Form.Field>
-									<Form.Label htmlFor="address">Endereço:</Form.Label>
-									<Form.Input inputSize="400px" name="address" />
-								</Form.Field>
-								{errors.address && <span>{errors.address.message}</span>}
-							</div>
-
-							<div>
-								<Form.Field>
-									<Form.Label htmlFor="number">Número:</Form.Label>
-									<Form.Input inputSize="114px" name="number" />
-								</Form.Field>
-								{errors.number && <span>{errors.number.message}</span>}
-							</div>
-
-							<div>
-								<Form.Field>
-									<Form.Label htmlFor="complement">Complemento:</Form.Label>
-									<Form.Input inputSize="100%" name="complement" />
-								</Form.Field>
-								{errors.complement && <span>{errors.complement.message}</span>}
+								{errors.client_cellphone && <span>{errors.client_cellphone.message}</span>}
 							</div>
 						</styled.FormLine>
 
 						<styled.FormLine>
 							<div>
 								<Form.Field>
-									<Form.Label htmlFor="neighborhood">Bairro:</Form.Label>
-									<Form.Input inputSize="740px" name="neighborhood" />
+									<Form.Label htmlFor="client_address">Endereço:</Form.Label>
+									<Form.Input inputSize="400px" name="client_address" />
 								</Form.Field>
-								{errors.neighborhood && (
-									<span>{errors.neighborhood.message}</span>
+								{errors.client_address && <span>{errors.client_address.message}</span>}
+							</div>
+
+							<div>
+								<Form.Field>
+									<Form.Label htmlFor="client_address_number">Número:</Form.Label>
+									<Form.Input inputSize="114px" name="client_address_number" />
+								</Form.Field>
+								{errors.client_address_number && <span>{errors.client_address_number.message}</span>}
+							</div>
+
+							<div>
+								<Form.Field>
+									<Form.Label htmlFor="client_address_complement">Complemento:</Form.Label>
+									<Form.Input inputSize="100%" name="client_address_complement" />
+								</Form.Field>
+								{errors.client_address_complement && <span>{errors.client_address_complement.message}</span>}
+							</div>
+						</styled.FormLine>
+
+						<styled.FormLine>
+							<div>
+								<Form.Field>
+									<Form.Label htmlFor="client_address_neighborhood">Bairro:</Form.Label>
+									<Form.Input inputSize="740px" name="client_address_neighborhood" />
+								</Form.Field>
+								{errors.client_address_neighborhood && (
+									<span>{errors.client_address_neighborhood.message}</span>
 								)}
 							</div>
 
 							<div>
 								<Form.Field>
-									<Form.Label htmlFor="cep">Cep:</Form.Label>
-									<Form.Input inputSize="300px" name="cep" />
+									<Form.Label htmlFor="client_zip_code">Cep:</Form.Label>
+									<Form.Input inputSize="300px" name="client_zip_code" />
 								</Form.Field>
-								{errors.cep && <span>{errors.cep.message}</span>}
+								{errors.client_zip_code && <span>{errors.client_zip_code.message}</span>}
 							</div>
 						</styled.FormLine>
 
@@ -135,8 +165,8 @@ export default function PurchaseConfirmation() {
 							</ul>
 						</styled.TextContainer>
 
-						<styled.FormButton type="submit">
-              Finalizar Pedido
+						<styled.FormButton type="submit" disabled={handleNewOrderMutation.isPending}>
+              {handleNewOrderMutation.isPending ? "Enviando Pedido" : "Finalizar Pedido"}
 						</styled.FormButton>
 					</styled.FormContainer>
 				</FormProvider>
